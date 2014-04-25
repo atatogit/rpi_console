06670a9f6bbdb9cc6718dc7e091d5c4fffd009e6
Atato Gomez <atatogitgomez@gmail.com>
2014-04-25 15:17:38 +0000

    Some changes to telnet module. Nothing major.

--- telnet.py
+++ telnet.py
@@ -7,19 +7,33 @@ __HOST = "192.168.1.1"
 __USER = "admin\n"
 __PASS = "Iseuqrom\n"
 
+__BEGIN = "#BEGINBEGINBEGIN"
+__END = "#ENDENDEND"
 
-def ExecuteCommand(command):
+def __ConnectAndLogIn():
     tn = telnetlib.Telnet(__HOST)
     tn.read_until("login: ")
     tn.write(__USER)
     tn.read_until("Password: ")
     tn.write(__PASS)
-    tn.write("%s\n" % command)
-    tn.write("exit\n")
     tn.read_very_eager()
+    return tn
+
+def ExecuteCommand(command):
+    tn = __ConnectAndLogIn()
+    tn.write("%s\n" % __BEGIN)
+    tn.write("%s\n" % command)
+    tn.write("%s\n" % __END)
+    tn.write("exit\n")
     return tn.read_all()
 
+def GetRemoteTextFile(path):
+    data = ExecuteCommand("cat %s" % path)
+    begin = data.rindex(__BEGIN) + len(__BEGIN) + 2
+    begin = data.index("\r\n", begin) + 2
+    end = data.index(__END, begin) - 2
+    end = data.rindex("\r\n", begin, end)
+    return data[begin:end]
+
 if __name__ == "__main__":
-    print "---"
-    print ExecuteCommand("ls /tmp/")
-    print "---"
+    print GetRemoteTextFile("/tmp/syslog.log")
